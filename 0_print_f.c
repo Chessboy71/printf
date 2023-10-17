911d42134b9e0894fa9a832217c2948b9765fd4e
chessboy71 <bentegrimohamed@gmail.com>
2023-10-17 23:16:22 +0100

    normalement finale task0

--- 0_print_f.c
+++ 0_print_f.c
@@ -1,84 +1,49 @@
-#include <stdarg.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * _printf - formatted output conversion and print data.
+ * @format: input string.
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: number of chars printed.
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-int print_digit(long n, int base)
-{
-	int count;
-	char *symbols;
-
-	symbols = "0123456789abcdef";
-	if (n < 0)
-	{
-		write(1, "-", 1);
-		return print_digit(-n, base) + 1;
-	}
-	else if (n < base)
-		return _putchar(symbols[n]);
-	else
-	{
-		count = print_digit(n / base, base);
-		return count + print_digit(n % base, base);
-	}
-}
-
-int print_str(char *s)
-{
-	int count;
-
-	count = 0;
-	while (*s)
-		count += write(1, s++, 1);
-	return count;
-}
-
-int print_format(char specifier, va_list ap)
-{
-	int count;
-
-	count = 0;
-	if (specifier == 'c')
-		count = _putchar(va_arg(ap, int));
-	else if (specifier == 's')
-		count = print_str(va_arg(ap, char *));
-	else if (specifier == 'd')
-		count = print_digit((long)va_arg(ap, int), 10);
-	else if (specifier == 'x')
-		count = print_digit((long)va_arg(ap, unsigned int), 16);
-	else if (specifier == '%')
-		count = _putchar('%');
-	else
-		count += write(1, &specifier, 1);
-	return count;
-}
-
 int _printf(const char *format, ...)
 {
-	va_list ap;
-	int count;
+	va_list args;
+	int i, p_char = 0;
 
-	va_start(ap, format);
-	count = 0;
-	while (*format)
+	va_start(args, format);
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+
+	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (*format == '%')
-			count += print_format(*++format, ap);
+		if (format[i] != '%')
+			_putchar(format[i]);
 		else
-			count += write(STDOUT_FILENO, format, 1);
-		++format;
+		{
+			i++;
+			if (format[i] == 'c')
+				_putchar((char)va_arg(args, int));
+			else if (format[i] == '%')
+				_putchar('%');
+			else if (format[i] == 's')
+			{
+				char *str = va_arg(args, char *);
+
+				if (str == NULL)
+				{
+					str = "(null)";
+				}
+				while (*str)
+				{
+					p_char += _putchar(*str);
+					str++;
+				}
+				p_char--;
+			}
+		}
+		p_char++;
 	}
-	va_end(ap);
-	return count;
+	va_end(args);
+	return (p_char);
 }
