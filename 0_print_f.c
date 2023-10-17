bd24b062b9f943acaf5bd5cabe1d0742e6645c18
RTamine01 <amne0002@gmail.com>
2023-10-17 09:16:55 -0700

    printf

--- 0_print_f.c
+++ 0_print_f.c
@@ -21,7 +21,6 @@ int _printf(const char *format, ...)
 			format++;
 			switch (*format)
 			{
-<<<<<<< HEAD
 				case 'c': {
 					char ch = (char)va_arg(args, char);
 
@@ -40,48 +39,11 @@ int _printf(const char *format, ...)
 					putchar('%');
 					putchar(*format);
 			}
-=======
-				case 'c':
-				{
-				char c = va_arg(arg_list, int);
-
-				p_char += _putchar(&c);
-				break;
-			}
-			case 's':
-			{
-				char *str = va_arg(arg_list, char *);
-
-				if (str != NULL)
-					_puts(str);
-				else
-					p_char += _puts("(NULL)");
-				break;
->>>>>>> 12530e3b0d4613d4ad1bf9f42147fab58c649b9b
-			}
 			else
 			{
-<<<<<<< HEAD
 				putchar(*format);
-=======
-				p_char += _putchar("%");
-				break;
->>>>>>> 12530e3b0d4613d4ad1bf9f42147fab58c649b9b
 			}
 			format++;
 		}
-<<<<<<< HEAD
-
-		va_end(args);
-=======
-		else
-		{
-			p_char += _putchar(format);
-			format++;
-		}
-	}
 	va_end(arg_list);
-	return (p_char);
->>>>>>> 12530e3b0d4613d4ad1bf9f42147fab58c649b9b
-}
 }
